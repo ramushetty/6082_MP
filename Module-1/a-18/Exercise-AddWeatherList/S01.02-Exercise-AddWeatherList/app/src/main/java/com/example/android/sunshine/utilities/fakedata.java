f2fd200843587554e8446147dc7d96c9d9ccae92
ramushetty <ummadishettyramu@msitprogram.net>
2019-03-12 20:44:21 +0530

    activity-18

---
+++ Module-1/a-18/Exercise-AddWeatherList/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/utilities/fakedata.java
@@ -0,0 +1,21 @@
+package com.example.android.sunshine.utilities;
+
+public class fakedata {
+    public static String[] sendfakedata() {
+        return new String[] {
+                "0 degrees",
+                "Captain Marvel = 60 degrees",
+                "Superman = 100 degrees",
+                "11 degrees",
+                "Thor",
+                "HULK",
+                "FLASH",
+                "Game of Thrones",
+                "Mission Impossible",
+                "jack reacher",
+                "fast and furious",
+                "die hard",
+                "jason bourn"
+        };
+    }
+}
